fff0db576d0a08974f1a07b9aa07c3321168af4c
Ashwin Kadammaje Giridhar <ashu.ashwinkg@gmail.com>
2022-07-20 23:32:30 -0500

    added the create movie

--- main.go
+++ main.go
@@ -4,21 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+//creating a struct of type Movie
 type Movies struct {
 	Id       string    `json:"id"`
 	Title    string    `json:"title"`
 	Isbn     string    `json:"isbn"`
-	Director *Director `json: "director"`
+	Director *Director `json:"director"`
 }
 
+//creating a struct of type Director
 type Director struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
 }
 
 var movies []Movies
@@ -57,6 +61,15 @@ func deleteMovieById(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func addNewMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movies
+	json.NewDecoder(r.Body).Decode(&movie)
+	random_number := strconv.Itoa(rand.Intn(100000))
+	movie.Id = random_number
+	movies = append(movies, movie)
+}
+
 func main() {
 	router := mux.NewRouter()
 	movies = append(movies, Movies{Id: "001", Title: "KGF-1", Isbn: "100", Director: &Director{FirstName: "Prashanth", LastName: "Neel"}})
@@ -64,6 +77,7 @@ func main() {
 	router.HandleFunc("/movies", getAllMovies).Methods("GET")
 	router.HandleFunc("/movies/{id}", getMovieById).Methods("GET")
 	router.HandleFunc("/movies/{id}", deleteMovieById).Methods("DELETE")
+	router.HandleFunc("/movies", addNewMovie).Methods("POST")
 	fmt.Println("Starting the server at 8080........")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
